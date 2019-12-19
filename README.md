ad1a64a19f0d41c9aa6f59d9770d4622eedc3ad6
Sidhartha Premkumar <spremkum@redhat.com>
2019-12-18 19:31:44 -0500

    test-1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # OpenShift-Build
+I want to build [this](https://cloud.google.com/cloud-build/) but in OpenShift.
---
+++ openshift-build/app.js
@@ -0,0 +1,22 @@
+var http = require('http')
+var createHandler = require('github-webhook-handler')
+var handler = createHandler({ path: '/webhook', secret: 'myhashsecret' })
+
+// http://localhost:1201
+
+http.createServer(function (req, res) {
+  handler(req, res, function (err) {
+    res.statusCode = 404
+    res.end('no such location')
+  })
+}).listen(1201)
+
+handler.on('error', function (err) {
+  console.error('Error:', err.message)
+})
+
+handler.on('push', function (event) {
+  console.log('Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref)
+})
