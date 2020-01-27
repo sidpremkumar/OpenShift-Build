6f98382b9d8b36eea27648d22cb1a08c63849684
Sid Premkumar <sid.premkumar@gmail.com>
2020-01-27 09:16:28 -0500

    Update test.py

--- test_data/test.py
+++ test_data/test.py
@@ -1,3 +1,10 @@
+import logging
+FORMAT = "[%(asctime)s] %(levelname)s: %(message)s"
+logging.basicConfig(format=FORMAT, level=logging.INFO)
+logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+logging.basicConfig(format=FORMAT, level=logging.WARNING)
+log = logging.getLogger('sync2jira.main')
+
 if __name__ == '__main__':
     # The Python File must print exactly 2 statements.
     # The first is failure/success
@@ -5,4 +12,5 @@ if __name__ == '__main__':
     print('success')
     print('Some more info')
     print('some more! capture this!')
+    log.info('some log info')
     # raise Exception("something")
