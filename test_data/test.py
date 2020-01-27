ede138299d0c76e1ccd8c68bba3a8de914f2a330
Sid Premkumar <sid.premkumar@gmail.com>
2020-01-27 09:08:05 -0500

    Update test.py

--- test_data/test.py
+++ test_data/test.py
@@ -1,7 +1,10 @@
+from time import sleep
+
 if __name__ == '__main__':
     # The Python File must print exactly 2 statements.
     # The first is failure/success
     # The second is any more data you want to return to the user
     print('success')
     print('Some more info')
+    sleep(100)
     raise Exception("something")
